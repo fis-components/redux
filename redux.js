c03e32fbb023d879aba8ed8856e22dae8ece85b4
2betop <liaoxuezhi@icloud.com>
2016-02-23 10:04:52 +0000

    based on https://github.com/fis-components/components/blob/master/packages/redux.json

--- redux.js
+++ redux.js
@@ -309,6 +309,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	   * @returns {void}
 	   */
 	  function replaceReducer(nextReducer) {
+	    if (typeof nextReducer !== 'function') {
+	      throw new Error('Expected the nextReducer to be a function.');
+	    }
+
 	    currentReducer = nextReducer;
 	    dispatch({ type: ActionTypes.INIT });
 	  }
